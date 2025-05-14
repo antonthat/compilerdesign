a5e9fd04721fbcb2e976bf0d8eaa827dd5c674b7
anton <antonthat49@gmail.com>
2025-05-14 02:59:06 +0200

    hopefully working run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 BIN_DIR="$(dirname "$0")/build/install/compiler/bin"
-$BIN_DIR/compiler "$@"
 
-gcc "$2" -o "${2%.s}"
+INPUT="$1"
+OUTPUT="$2"
+ASM_FILE="${OUTPUT}.s"
+
+$BIN_DIR/compiler "$INPUT" "$ASM_FILE"
+
+gcc "$ASM_FILE" -o "$OUTPUT"
